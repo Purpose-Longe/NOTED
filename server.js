430e4926297913cb5e074b6e5e281729a31f66f9
Purpose Longe <purposelonge@gmail.com>
2025-10-20 00:06:52 -0800

    database connection completed

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const connectDB = require("./src/config/db");
-connectDB();
+require('dotenv').config();
+
 const app = express();
 app.use(express.json());
+connectDB();
+
 app.get("/api/health", (req, res) => {
   res.json({ status: "Server is working fine" });
 });
 app.get("/api/info", (req, res) => {
   res.send("Welcome to NOTED API");
 });
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
